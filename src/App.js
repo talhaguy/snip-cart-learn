d71f9b02ced4b2c4992813ff024068987e24ff48
Muhammad Talha <talhaguy@gmail.com>
2020-11-07 11:12:02 -0800

    show mini cart

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@
 function App() {
   return (
     <div>
+      <header>
+        <button className="snipcart-checkout">Cart</button><br />
+        Items: <span className="snipcart-items-count"></span><br />
+        Amount: <span className="snipcart-total-price"></span>
+      </header>
+
       <h2>Soccer ball</h2>
-      <img src="/logo512.png" />
+      <img src="/logo192.png" />
       <p>This is a wonderful soccer ball.</p>
       <p>$19.99</p>
       <button className="snipcart-add-item"
@@ -15,6 +21,7 @@ function App() {
         data-item-name="Soccer ball">
         Add to cart
       </button>
+
     </div>
   );
 }
